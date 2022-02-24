470ee01b9c5c5aadbc004dbfb62c7b8673963e6b
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-02-24 17:49:47 +0100

    add script init and reorganize repo (#1)

---
+++ utils/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Clone training repo
+REPO_URL=https://github.com/InseeFrLab/formation-r-lissage-spatial
+CLONE_DIR=/home/rstudio/formation
+git clone --depth 1 $REPO_URL $CLONE_DIR
+cp $CLONE_DIR
+
+# Copy training data
+mkdir data
+mc cp s3/projet-formation/r-lissage-spatial/* data/

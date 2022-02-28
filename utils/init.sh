425a70df4b9fc2d8c8cca537f26849f5251b81a7
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-02-28 17:31:21 +0100

    fix minio path

--- utils/init.sh
+++ utils/init.sh
@@ -3,7 +3,7 @@
 # Copy training data
 DATA_DIR=/home/rstudio/formation/data
 mkdir $DATA_DIR
-mc cp s3/diffusion/projet_formation/r-lissage-spatial/* $DATA_DIR
+mc cp s3/projet-formation/r-lissage-spatial/* $DATA_DIR
 
 # Launch RStudio in the right project
 echo \

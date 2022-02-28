e8ea3851787d7931754a33edf268c83cc3f6cc32
avouacr <avouacr@pm.me>
2022-02-28 16:55:44 +0000

    fix permissions

--- utils/init.sh
+++ utils/init.sh
@@ -4,20 +4,9 @@
 REPO_URL=https://github.com/InseeFrLab/formation-r-lissage-spatial
 CLONE_DIR=/home/rstudio/formation
 git clone --depth 1 $REPO_URL $CLONE_DIR
-cp $CLONE_DIR
+chown -R rstudio:users $WORK_DIR
+cd $CLONE_DIR
 
 # Copy training data
-DATA_DIR=$CLONE_DIR/data
-mkdir $DATA_DIR
-mc cp s3/projet-formation/r-lissage-spatial/* $DATA_DIR/
-
-# Launch RStudio in the right project
-echo \
-"
-setHook('rstudio.sessionInit', function(newSession) {
-    if (newSession && identical(getwd(), path.expand('~')))
-    {
-        rstudioapi::openProject('$CLONE_DIR')
-        }
-        }, action = 'append')
-        " >> /home/rstudio/.Rprofile
+mkdir data
+mc cp s3/projet-formation/r-lissage-spatial/* data/

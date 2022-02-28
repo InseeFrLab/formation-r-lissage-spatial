436cbc1750c7d30f5ce562a627c9480bd3eef5ec
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-02-28 17:57:57 +0100

    Update init.sh

--- utils/init.sh
+++ utils/init.sh
@@ -4,7 +4,7 @@
 REPO_URL=https://github.com/InseeFrLab/formation-r-lissage-spatial
 CLONE_DIR=/home/rstudio/formation
 git clone --depth 1 $REPO_URL $CLONE_DIR
-chown -R rstudio:users $WORK_DIR
+chown -R rstudio:users $CLONE_DIR
 cd $CLONE_DIR
 
 # Copy training data
